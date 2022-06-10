e9288478c330175f8e1f86bf80c10d720a5ff321
Fábio Santos <fabio3268@gmail.com>
2022-06-10 07:54:12 -0300

    Semana 02

--- 05-heranca/index.php
+++ 05-heranca/index.php
@@ -2,3 +2,18 @@
 
 require __DIR__ . "/source/autoload.php";
 
+$user = new \Source\General\User(
+    "Fábio",
+"fabio@gmail.com"
+);
+
+var_dump($user);
+
+$doctor = new \Source\Hospital\Doctor(
+    "Dráuzio Varela",
+    "varela@gmail.com",
+    "324567",
+    "Oncologista"
+);
+
+var_dump($doctor);
