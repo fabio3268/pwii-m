b1c54c5ee600eb792d4247bee5ce10eacafaa057
Fábio Santos <fabio3268@gmail.com>
2023-03-23 11:05:27 -0300

    Definição de Classes e Objetos

--- source/User.php
+++ source/User.php
@@ -1,3 +1,34 @@
 <?php
 
+class User
+{
+    public $name;
+    public $email;
+
+    public function __construct( $name = null, $email = null)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+}
 
