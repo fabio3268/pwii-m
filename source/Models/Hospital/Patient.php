d4e837b70a3f079eba4447ee188bf461b9c09734
Fábio Santos <fabio3268@gmail.com>
2023-03-31 08:23:11 -0300

    Herança

--- source/Models/Hospital/Patient.php
+++ source/Models/Hospital/Patient.php
@@ -6,5 +6,14 @@ use Source\Models\User;
 
 class Patient extends User
 {
+    private $dateOfBirth;
     private $chart;
+
+    public function __construct($name = null, $email = null,
+        $password = null, $dateOfBirth = null, $chart = null)
+    {
+        parent::__construct($name, $email, $password);
+        $this->chart = $chart;
+        $this->dateOfBirth = $dateOfBirth;
+    }
 }
