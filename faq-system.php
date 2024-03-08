a51b7d6e77af0b149f7591bb82013d26596e2ff7
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-08 07:57:22 -0300

    User Atualizada pasta Models

--- faq-system.php
+++ faq-system.php
@@ -4,8 +4,12 @@ require __DIR__ . "/source/autoload.php";
 //require __DIR__ . "/source/Models/Faq/Question.php";
 //require __DIR__ . "/source/Models/Faq/Type.php";
 
+use Source\Models\User;
 use Source\Models\Faq\Question;
 use Source\Models\Faq\Type;
+
+$user = new User();
+var_dump($user);
 
 $question = new Question();
 var_dump($question);
